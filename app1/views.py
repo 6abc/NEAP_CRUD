a42ed847abba1f026002a1c9f77c50dcb0ba99ec
Ashish <97246854+6abc@users.noreply.github.com>
2024-11-24 00:30:01 +0530

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -6,9 +6,16 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 
 # Create your views here.
-def HomeView(request):
-    data = Movie.objects.all()
-    return render(request, 'home.html', {'data_dict': data})
+class HomeView(View):
+    template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        try:
+            data_dict = Movie.objects.all()
+        except OperationalError:
+            data_dict = []  # Fallback if the table does not exist
+
+        return render(request, self.template_name, {"data_dict": data_dict})
 
 @method_decorator(staff_member_required, name='dispatch')
 class MovieView(CRUDView):
